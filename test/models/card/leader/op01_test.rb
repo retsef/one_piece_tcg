565bbc393efa3ffd551cae3d30ee99ef2b376a79
Roberto Scinocca <roberto.scinocca@gmail.com>
2024-06-01 08:21:48 +0200

    add more cards effect/trigger to test

---
+++ test/models/card/leader/op01_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class Card::Leader::OP01Test < ActiveSupport::TestCase
+  EFFECT = [
+    "[DON!! x1] [Your Turn] All of your Characters gain +1000 power.",
+    "[Activate: Main] [Once Per Turn] ➁ (You may rest the specified number of DON!! cards in your cost area.): If you have 5 Characters, return 1 of your Characters to the owner's hand. Then, play up to 1 Character with a cost of 5 or less from your hand that is a different color than the returned Character.",
+    "[Activate: Main] [Once Per Turn] ➃ (You may rest the specified number of DON!! cards in your cost area.): Set up to 1 of your {Supernovas} or {Straw Hat Crew} type Character cards with a cost of 5 or less as active. It gains +1000 power during this turn.",
+    "[Activate: Main] [Once Per Turn] You can trash 1 {Land of Wano} type card from your hand: Set up to 2 of your DON!! cards as active.",
+    "[DON!! x2] [When Attacking] ➀ (You may rest the specified number of DON!! cards in your cost area.): Reveal 1 card from the top of your deck. If that card is a {The Seven Warlords of the Sea} type Character card with a cost of 4 or less, you may play that card rested.",
+    "[DON!! x1] [Your Turn] [Once Per Turn] When your opponent's Character is K.O.'d, add up to 1 DON!! card from your DON!! deck and set it as active.",
+    "[DON!! x1] When you activate an Event, you may draw 1 card if you have 4 or less cards in your hand and haven't drawn a card using this Leader's effect during this turn.",
+    "[Your Turn] If you have 10 DON!! cards on your field, give all of your opponent's Characters −1000 power."
+  ].freeze
+
+  EFFECT.each do |effect|
+    test "parse effect: #{effect}" do
+      parsed = Card::Leader::Effect.parse(effect)
+
+      assert_not_nil parsed
+    end
+  end
+end
---
+++ test/models/card/leader/op02_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class Card::Leader::OP02Test < ActiveSupport::TestCase
+  EFFECT = [
+    "[End of Your Turn] Add 1 card from the top of your Life cards to your hand.",
+    "[Your Turn] When this Leader or any of your Characters is given a DON!! card, give up to 1 of your opponent's Characters with a cost of 7 or less −1 cost during this turn.",
+    "[Activate: Main] [Once Per Turn] If you have 1 or less Characters, the next time you play a {Land of Wano} type Character card with a cost of 3 or more from your hand during this turn, the cost will be reduced by 1.",
+    "[Once Per Turn] When you play a Character with no base effect from your hand, if you have 3 or less Characters, set up to 2 of your DON!! cards as active.",
+    "[End of Your Turn] If you have 0 cards in your hand, draw 2 cards.",
+    "[Your Turn] [Once Per Turn] When a DON!! card on the field is returned to your DON!! deck, this Leader gains +1000 power during this turn.",
+    "[When Attacking] DON!! −4 (You may return the specified number of DON!! cards from your field to your DON!! deck.): K.O. up to 1 of your opponent's Characters with a cost of 3 or less. Then, this Leader gains +1000 power during this turn.",
+    "[DON!! x1] [Activate: Main] [Once Per Turn] Give up to 1 of your opponent's Characters −1 cost during this turn. Then, if there is a Character with a cost of 0, this Leader gains +1000 power during this turn."
+  ].freeze
+
+  EFFECT.each do |effect|
+    test "parse effect: #{effect}" do
+      parsed = Card::Leader::Effect.parse(effect)
+
+      assert_not_nil parsed
+    end
+  end
+end
