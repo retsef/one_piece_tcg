aeaa9de47dffc546e9179b1fed90ab1374607e0c
Roberto Scinocca <roberto.scinocca@gmail.com>
2024-11-11 00:14:47 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  mount ActionCable.server => '/cable'
+  get 'up', to: 'rails/health#show', as: :rails_health_check
+  mount ActionCable.server, at: '/cable'
 
   draw :auth
 
@@ -12,9 +12,10 @@ Rails.application.routes.draw do
   draw :api
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root controller: 'admin/cards', action: :index
 
-  get 'manifest', to: 'pwa#manifest'
+  get 'manifest', to: 'pwa#manifest', defaults: { format: 'json' }
+  get 'service_worker', to: 'pwa#service_worker', defaults: { format: 'js' }
 
   with_options via: :all do
     match '/404', to: 'errors#not_found'
